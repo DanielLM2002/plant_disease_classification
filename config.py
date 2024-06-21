b4ac91a1de00d2741735c0934a7ff94d91ebc2e4
Israel Lopez <israel.lopez@ucr.ac.cr>
2024-06-20 19:33:05 -0600

    Código más limpio para entrenamiento

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+# MODEL PARAMETERS
+USE_UNET = True
+USE_DENOISING_AUTOENCODER = False
+AE_TRANSFORMS = 'bce'
+CNN_BATCH_SIZE = 32
+AE_BATCH_SIZE = 4
+ENCODER_CNN_BATCH_SIZE = 4
+TRAIN_ENCODER_WEIGHTS = False
+
+
+# DATASET PARAMETERS
+UNLABELED_SET_SIZE = 0.8
+LABELED_TRAIN_SET_ABSOLUTE_SIZE = 0.1
+LABELED_TEST_SET_ABSOLUTE_SIZE = 0.1
+
+BASE_DIR_RAW = os.path.join('Plant_leave_diseases_dataset', 'original')
+BASE_DIR_NOISY = os.path.join('Plant_leave_diseases_dataset', 'with_noise')
+BASE_DIR_MODELS = 'best_models'
+
+os.makedirs(BASE_DIR_MODELS, exist_ok=True)
+
+AUTOENCODER_SAVE_PATH = \
+    os.path.join('best_models', f'h1_{int(UNLABELED_SET_SIZE*100)}-{int(LABELED_TRAIN_SET_ABSOLUTE_SIZE*100)}-{int(LABELED_TEST_SET_ABSOLUTE_SIZE*100)}_Autoencoder.pth')
+DENOISING_AUTOENCODER_SAVE_PATH = \
+    os.path.join('best_models', f'h2_{int(UNLABELED_SET_SIZE*100)}-{int(LABELED_TRAIN_SET_ABSOLUTE_SIZE*100)}-{int(LABELED_TEST_SET_ABSOLUTE_SIZE*100)}_DenoisingAutoencoder.pth')
+CNN_SAVE_PATH = \
+    os.path.join('best_models', f'h1_{int(UNLABELED_SET_SIZE*100)}-{int(LABELED_TRAIN_SET_ABSOLUTE_SIZE*100)}-{int(LABELED_TEST_SET_ABSOLUTE_SIZE*100)}_classifierA.pth')
+CNN_ENCODER_SAVE_PATH = \
+    os.path.join('best_models', f'h1_{int(UNLABELED_SET_SIZE*100)}-{int(LABELED_TRAIN_SET_ABSOLUTE_SIZE*100)}-{int(LABELED_TEST_SET_ABSOLUTE_SIZE*100)}_classifier{"B" if not TRAIN_ENCODER_WEIGHTS else "B"}.pth')
+
